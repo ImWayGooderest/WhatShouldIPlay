9bf56fde8003e6a12836a28a348d1e036f7f34f5
Skyler Lister Aley <skylerlisteraley@gmail.com>
2016-04-18 23:05:53 -0700

    First version of update steam games list
    also removed API key hardcoding and added setup modal on first run

--- js/index.js
+++ js/index.js
@@ -1,38 +1,54 @@
 var $currentUserSteam = ""; 
 var $currentUsername = "";
-var $exists = false;
+var $steamAPI = "";
+var $giantBombAPI = "";
 
 $(document).ready(function() {
-  "use strict";
+	"use strict";
 
-  $("#signup").click(function() {
-	$("#signinButton").hide();
-	$("#registerButton").show();
-	$("#inputDIVSteamID").show();
-	$("#registerSignin-title").empty().append("Sign Up");
-	$("#registerSignIn").modal();
-	$("#errorMsg").hide();
-  });
+	if($currentUserSteam == ""){
+		$("#update").hide();
+	}
 
-  $("#signin").click(function() {
-	$("#registerButton").hide();
-	$("#signinButton").show();
-	$("#inputDIVSteamID").hide();
-	$("#registerSignin-title").empty().append("Sign In");
-	$("#registerSignIn").modal();
-	$("#errorMsg").hide();
-  });
+	if($giantBombAPI == ""){
+		$("#apiModal").modal();
+	}
 
-  	$("#registerButton").click(function() {
+	$("#setupButton").click(function() {
+		var apiData = _.object($("#apiModal-form").serializeArray().map(function(v) {return [v.name, v.value];} ));
+		$steamAPI = apiData.steamAPIModal;
+		$giantBombAPI = apiData.giantBombAPIModal;
+		var $greeting = '<span class="text-primary" id="greeting">Steam API: ' + $steamAPI +'<br> Giant Bomb API: '+ $giantBombAPI +'</li>';
+		$("#navbar").append($greeting);
+		$.post("http://localhost:3000/API", {"steamAPI": apiData.steamAPIModal}, function(data){});
+		$("#apiModal").modal("hide");
+	});
 
-	    var registrationData = _.object($("#registerSignIn-form").serializeArray().map(function(v) {return [v.name, v.value];} ));  //returns form values as key value pairs
-	    $.post("http://localhost:3000/exists", {"username": registrationData.email.toLowerCase()}, function(data) {
-       		if(data.length != 0){
-       			console.log(data);
-       			console.log("exists");
-       			$("#errorMsg").show();
-		    	$("#errorMsg").text("Sign up failed. Account already exists, please try again!");
-			    $("#errorMsg").effect("shake");
+	$("#signup").click(function() {
+		$("#signinButton").hide();
+		$("#registerButton").show();
+		$("#inputDIVSteamID").show();
+		$("#registerSignin-title").empty().append("Sign Up");
+		$("#registerSignIn").modal();
+		$("#errorMsg").hide();
+	});
+
+	$("#signin").click(function() {
+		$("#registerButton").hide();
+		$("#signinButton").show();
+		$("#inputDIVSteamID").hide();
+		$("#registerSignin-title").empty().append("Sign In");
+		$("#registerSignIn").modal();
+		$("#errorMsg").hide();
+	});
+
+	$("#registerButton").click(function() {
+		var registrationData = _.object($("#registerSignIn-form").serializeArray().map(function(v) {return [v.name, v.value];} ));  //returns form values as key value pairs
+		$.post("http://localhost:3000/exists", {"username": registrationData.email.toLowerCase()}, function(data) {
+				if(data.length != 0){
+					$("#errorMsg").show();
+		    		$("#errorMsg").text("Sign up failed. Account already exists, please try again!");
+			    	$("#errorMsg").effect("shake");
 			}
 			else{
 				$.post("http://localhost:3000/signup", {"username": registrationData.email.toLowerCase(), "password": registrationData.password, "steamID" : registrationData.steamID}, function(data) { 
@@ -40,16 +56,15 @@ $(document).ready(function() {
 			        $("#inputEmail").val("");
 			        $("#inputPassword").val("");
 			        $("#inputSteamID").val("");
-	        	});
+		    	});
 			}
-	    });
-  	});
+		});
+	});
 
   	$("#signinButton").click(function() {
 	    var signinData = _.object($("#registerSignIn-form").serializeArray().map(function(v) {return [v.name, v.value];} ));//converts form data to associative array
 	    $.post("http://localhost:3000/signin", { "username": signinData.email.toLowerCase(), "password": signinData.password }, function(data) {
        		if(data.length != 0){
-       			console.log(data);
 	        	$currentUsername = data[0].username;
 	        	$currentUserSteam = data[0].steamID;
 
@@ -61,8 +76,9 @@ $(document).ready(function() {
 		        $("#signin").hide();
 		        $("#logOut").show();
 
-		        var $greeting = '<span class="text-primary" id="greeting">Hello, ' + $currentUsername + $currentUserSteam +'!</li>';
+		        var $greeting = '<span class="text-primary" id="greeting">Hello, ' + $currentUsername +'!</li>';
 		        $("#navbar").append($greeting);
+		        showSteamGames();
        		}
        		else{
        			$("#errorMsg").show();
@@ -71,7 +87,30 @@ $(document).ready(function() {
        		}
 	    });
   	});
+	
+	function showSteamGames(){
+		if($currentUserSteam != ""){
+			$.post("http://localhost:3000/update", { "steamID": $currentUserSteam}, function(data) {
+				if(data.length != 0){
+					var text = 'My Steam Games: <br><table class="table table-hover">';
+					for(var i = 0; i < data.games.length; i++){
+						text += '<tbody><tr><td><img src="http://media.steampowered.com/steamcommunity/public/images/apps/'+data.games[i].appid+'/'+data.games[i].img_logo_url+'.jpg"/>';
+						var qouted = data.games[i].name;
+						qouted = qouted.replace(/ /g, "+");
+						text += '<td><a href="http://www.giantbomb.com/api/search/?api_key='+$giantBombAPI+'&format=json&resources=game&query='+qouted+'" target="_blank">'+data.games[i].name+'</a>';
+						text += '<td>Time Played: '+data.games[i].playtime_forever+'minutes';
+						text += '<td><button type="button" class="btn btn-primary btn-lg">Giant Bomb Update</button>'
+					}
+					$("#gameList").append(text);
+				}
+			});
+		}	
+	}
 
+	$("#update").click(function() {
+		showSteamGames();
+	});
+	
 	$("#logOut").click(function() {
 	    $currentUsername = "";
 	    $currentUserSteam = "";
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var mongojs = require('mongojs');
 var db = mongojs('wsip');
 var users = db.collection('users');
 var userGames = db.collection('userGames');
+var steamKey;
 
 app.use(express.static(__dirname));
 // Create our Express-powered HTTP server
@@ -22,10 +23,10 @@ app.listen(3000, function() {
 
 app.use(bodyParser());
 
-var steamKey = "91B7AEA8FECAFC8C2F96FC4A0E2BF0FA";
-var steamID = "76561197962290933"
-var url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
-    +"?key="+steamKey+"&steamid="+steamID+"&include_appinfo=1&format=json"
+app.post('/API',function(req,res){ 
+    steamKey = req.body.steamAPI;
+    console.log("Steam Key Setup: "+steamKey);
+});
 
 app.post('/signup',function(req,res){ 
     users.save(req.body);
@@ -35,7 +36,6 @@ app.post('/exists',function(req,res){
     console.log(req.body);
     users.find(req.body, function(err, doc){
         if(doc != null){
-            console.log("doc: "+JSON.stringify(doc));
             res.json(doc);
         }
     });
@@ -44,29 +44,27 @@ app.post('/exists',function(req,res){
 app.post('/signin',function(req,res){
     users.find(req.body, function(err, doc){
         if(doc != null){
-            console.log("doc: "+JSON.stringify(doc));
             res.json(doc);
         }
     });
 
 });
 
-request({
-    url: url,
-    json: true
-}, function (error, response, body) {
+app.post('/update',function(req,res){
+    var steamID = req.body.steamID;
+    var url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/" +"?key="+steamKey+"&steamid="+steamID+"&include_appinfo=1&format=json"
+    request({url: url, json: true}, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var tempSteam = body.response;
+            tempSteam.steamID = steamID;
+            userGames.update({"steamID": steamID},tempSteam, {upsert: true});
+            res.json(tempSteam);
+        }
+        else{
+            console.log("ERROR:"+error);
+        }
+    })
+});
 
-    if (!error && response.statusCode === 200) {
-        var tempSteam = body.response;
-        tempSteam.steamID = steamID;
 
-        userGames.save(tempSteam);
-        //$("wsip.userGames").find({}, {"games.name": 1}, function(r){
-        //    console.log();
-        //});
-    }
-    else{
-        console.log("ERROR:"+error);
-    }
-})
 
