1bb842fc73dd40eb37a60c23262f26ae2713cedb
Brendon Hollingsworth <tucaeg@yahoo.com>
2016-05-17 20:35:55 -0700

    ADDED ONLY SHOW GAMES OWNED CHECKBOX AND IT SEEMS TO WORK WOOOOO

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ var $allConcepts = "";
 var $allThemes = "";
 var $allGamesHome = "";
 var $usersGames = "";
+var $gamesList = "";
 
 $(document).ready(function () {
 	"use strict";
@@ -503,22 +504,44 @@ function searchGenre(genre) {
 	$.post("http://localhost:3000/searchGenre", {
 		"genre": genre
 	}, function (data) {
-		$("#gameList").empty();
 		renderTable(data, genre, "Genre");
 	});
 }
+function filterTableToOwnedGames(data) {
+	var filteredGames = [];
+	var temp;
+	$.each($usersGames, function (index, value){
+      temp = _.findWhere(data, {steamAppID: value["appid"].toString()});
+      if(temp)
+        filteredGames.push(temp)
+    });
+	return filteredGames;
+}
+
+$(document).on('change','#onlyOwnedGames',function(){
+	if(this.checked)
+		$('#filteredTable').DataTable().clear().rows.add(filterTableToOwnedGames($gamesList)).draw();
+	else
+		$('#filteredTable').DataTable().clear().rows.add($gamesList).draw();
+
+});
+
 
 function renderTable(data, name, type) {
+	$gamesList = data;
+	$("#gameList").empty();
 	type = type + ": ";
 	var text = '<h1 style="color:#dd4814">' + type + name + '</h1><br><table width="100%" class="table text-center table-hover" id="filteredTable">';
 	if (data.length != 0) {
-		data = shuffle(data);
-
+		text += '<input type="checkbox" id="onlyOwnedGames" > <label for="onlyOwnedGamescbox">Only List Games You Own (beta)</label>';
 		text += '<thead><tr><th class="text-center">Game Art</th>';
 		text += '<th class="text-center">Giant Bomb ID</th>';
 		text += '<th class="text-center">Title</th>';
 		text += '<th class="text-center">Description</th>';
 		text += '<th class="text-center">Launch Game</th>';
+
+
+
 		$("#gameList").append(text);
 		$('#filteredTable').DataTable({
 			"processing": true,
@@ -559,13 +582,6 @@ function renderTable(data, name, type) {
 
 			}]
 		});
-		// for (var i = 0; i < data.length; i++) {
-		// 	text += '<tbody><tr><td><a href=# onclick="view(' + data[i].id + ')"><img class="img box-shadow--6dp" src="' + data[i].image.small_url + '" width="300"></a>';
-		// 	text += '<td>' + data[i].id;
-		// 	text += '<td>' + data[i].name;
-		// 	text += '<td>' + data[i].deck;
-		// 	text += '<td><a href="steam://run/' + data[i].steamAppID + '"><button type="button" class="btn btn-primary btn-lg">Launch Game</button>';
-		// }
 	}
 
 }
@@ -574,7 +590,6 @@ function searchDeveloper(developers) {
 	$.post("http://localhost:3000/searchDevelopers", {
 		"developer": developers
 	}, function (data) {
-		$("#gameList").empty();
 		renderTable(data, developers, "Developer");
 	});
 }
@@ -583,7 +598,6 @@ function searchTheme(theme) {
 	$.post("http://localhost:3000/searchTheme", {
 		"theme": theme
 	}, function (data) {
-		$("#gameList").empty();
 		renderTable(data, theme, "Theme")
 	});
 }
@@ -600,7 +614,6 @@ function searchConcept(concept) {
 	$.post("http://localhost:3000/searchConcept", {
 		"concept": concept
 	}, function (data) {
-		$("#gameList").empty();
 		renderTable(data, concept, "Concept")
 	});
 }
